01b7a12bec2ab5f04a8d0f668d08c6c6b07257f4
v-lavs <lavskavanda@gmail.com>
2024-10-16 14:46:10 +0300

    popup contacts added form css

--- src/js/main.js
+++ src/js/main.js
@@ -8,13 +8,16 @@ $(document).ready(function () {
 
 // MOBILE MENU
     const sidebar = document.querySelector('.sidebar');
+    const headerSbOp = document.querySelector('.header');
     const btnBurger = document.querySelector('.btn-burger');
     const menuLinks = document.querySelectorAll('.sidebar .menu__link');
     const overlay = document.querySelector('.backdrop');
+    const btnCallSb = document.querySelector('.btn_call');
 
     btnBurger.addEventListener('click', function (e) {
         e.preventDefault();
         sidebar.classList.toggle('open');
+        headerSbOp.classList.toggle('open');
         overlay.classList.toggle('el-visible');
     });
 
@@ -26,6 +29,11 @@ $(document).ready(function () {
     });
 
     overlay.addEventListener('click', function () {
+        sidebar.classList.remove('open');
+        overlay.classList.remove('el-visible');
+    });
+
+    btnCallSb.addEventListener('click', function () {
         sidebar.classList.remove('open');
         overlay.classList.remove('el-visible');
     });
@@ -53,7 +61,6 @@ $(document).ready(function () {
 
     window.addEventListener('resize', updateStickyBannerOffset);
     window.addEventListener('load', updateStickyBannerOffset);
-
 
     const bannerThumb = document.querySelector(".section-banner__thumb");
     const bannerTitle = document.querySelector('.page-title');
@@ -116,7 +123,6 @@ $(document).ready(function () {
             },
         });
     }
-
     if (document.querySelector('.green-slider')) {
         const greenSlider = new Swiper('.green-slider', {
             pagination: {
@@ -129,7 +135,6 @@ $(document).ready(function () {
             },
         });
     }
-
     if (document.querySelector('.slider-4-col')) {
         const slider4Col = new Swiper('.slider-4-col', {
             slidesPerView: 4,
@@ -179,6 +184,116 @@ $(document).ready(function () {
             document.getElementById(targetId).classList.add('active');
         });
     });
+
+    // ADDED COLOR LABEL CHECKED CHECKBOX END RADIOBUTTON
+    function setParentColor(input) {
+        if (input.checked) {
+            input.parentElement.style.color = '#FFFFFF';
+        } else {
+            input.parentElement.style.color = '#45655D';
+        }
+    }
+
+    document.querySelectorAll('input[type="checkbox"], input[type="radio"]').forEach(function (input) {
+        setParentColor(input);
+        input.addEventListener('click', function () {
+            if (input.type === 'radio') {
+                document.querySelectorAll(`input[name="${input.name}"]`).forEach(function (radio) {
+                    setParentColor(radio);
+                });
+            } else {
+                setParentColor(input);
+            }
+        });
+    });
+
+// POPUP
+    const popupCont = document.querySelector(".popup_contacts");
+    const btnCall = document.querySelector(".btn_call");
+    const contactsClose = document.querySelector('.popup_contacts .popup_close');
+    const backdrop = document.querySelector('.backdrop');
+
+    btnCall.addEventListener('click', () => {
+        popupCont.classList.add('open_modal');
+        backdrop.classList.toggle('el-visible');
+    });
+
+    contactsClose.addEventListener('click', () => {
+        popupCont.classList.remove('open_modal');
+        backdrop.classList.remove('el-visible');
+    });
+    backdrop.addEventListener('click', () => {
+        popupCont.classList.remove('open_modal');
+    });
+
+
+//    MULTI STEP FORM
+    const msForm = document.querySelector('.multi-step-form');
+    if (msForm) {
+        const stepsCircle = msForm.querySelectorAll('.progress-step');
+        const stepContent = msForm.querySelectorAll('.form__step');
+
+        const btnsNext = msForm.querySelectorAll('.btn_next');
+        const prevBtns = msForm.querySelectorAll('.form__nav-link');
+
+        stepsCircle[0].classList.add('progress-step_active');
+        stepContent[0].classList.add('active');
+        prevBtns[0].classList.add('show')
+
+        btnsNext.forEach(btn => btn.addEventListener(
+            'click', (e) => {
+                const activeIndex = Array.from(stepContent).findIndex(el => el.classList.contains('active'));
+                stepContent.forEach(el => el.classList.remove('active'))
+
+                const activeContent = stepContent[activeIndex + 1];
+                const activeCircle = stepsCircle[activeIndex + 1];
+                if (activeContent) {
+                    activeContent.classList.add('active')
+                }
+                if (activeCircle) {
+                    activeCircle.classList.add('progress-step_active')
+                }
+                prevBtns.forEach((el, index) => {
+                    if (index <= activeIndex + 1) {
+                        el.classList.add('show');
+                    } else {
+                        el.classList.remove('show');
+                    }
+                })
+            }
+        ));
+
+        prevBtns.forEach(btn => btn.addEventListener(
+            'click', (e) => {
+                const activeIndex = Array.from(prevBtns).findIndex(el => el === e.target);
+                console.log(activeIndex)
+                stepContent.forEach(el => el.classList.remove('active'))
+
+                stepsCircle.forEach((el, index) => {
+                    if (index > activeIndex) {
+                        el.classList.remove('progress-step_active')
+                    } else {
+                        el.classList.add('progress-step_active')
+                    }
+                })
+
+                prevBtns.forEach((el, index) => {
+                    if (index > activeIndex) {
+                        el.classList.remove('show');
+                    } else {
+                        el.classList.add('show');
+                    }
+                })
+
+                const activeContent = stepContent[activeIndex];
+                if (activeContent) {
+                    activeContent.classList.add('active')
+                }
+            }
+        ));
+
+
+    }
 });
 
 
