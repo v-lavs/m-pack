d0e66a5f15d6104e06f2b5e7267fc2a2410d3c40
v-lavs <lavskavanda@gmail.com>
2024-10-31 11:18:26 +0200

    comit

---
+++ dist/main.js
@@ -0,0 +1,361 @@
+/*
+* to include js file write: `//= include ./path-to-file`
+* */
+
+// CUSTOM SCRIPTS
+document.addEventListener('DOMContentLoaded', function () {
+
+// MOBILE MENU
+    const sidebar = document.querySelector('.sidebar');
+    const headerSbOp = document.querySelector('.header');
+    const btnBurger = document.querySelector('.btn-burger');
+    const menuLinks = document.querySelectorAll('.sidebar .menu__link');
+    const overlay = document.querySelector('.sidebar__overlay');
+    const btnCallSb = document.querySelector('.btn_call');
+
+    btnBurger.addEventListener('click', function (e) {
+        e.preventDefault();
+        sidebar.classList.toggle('open');
+        headerSbOp.classList.toggle('open');
+        overlay.classList.toggle('el-visible');
+    });
+
+    menuLinks.forEach(function (link) {
+        link.addEventListener('click', function () {
+            sidebar.classList.remove('open');
+            overlay.classList.remove('el-visible');
+        });
+    });
+
+    overlay.addEventListener('click', function () {
+        sidebar.classList.remove('open');
+        overlay.classList.remove('el-visible');
+    });
+
+    btnCallSb.addEventListener('click', function () {
+        sidebar.classList.remove('open');
+        overlay.classList.remove('el-visible');
+    });
+
+//STICKY HEADER PAGE
+    const stickyElement = document.querySelector('.page__header');
+    let stickyOffset = 0;
+
+    function updateStickyBannerOffset() {
+        const pageBanner = document.querySelector('.section-banner');
+        if (pageBanner) {
+            const bannerH = pageBanner.offsetHeight;
+            const stickyH = stickyElement.offsetHeight;
+
+            stickyOffset = bannerH - stickyH;
+
+            if (window.pageYOffset >= stickyOffset) {
+                stickyElement.classList.add('sticky');
+                stickyOffset = stickyElement.offsetTop;
+            } else {
+                stickyElement.classList.remove('sticky');
+            }
+        }
+    }
+
+    window.addEventListener('resize', updateStickyBannerOffset);
+    window.addEventListener('load', updateStickyBannerOffset);
+
+    const bannerThumb = document.querySelector(".section-banner__thumb");
+    const bannerTitle = document.querySelector('.page-title');
+
+    window.addEventListener("scroll", () => {
+        updateStickyBannerOffset()
+
+        if (window.scrollY > 0) {
+            bannerThumb.classList.add("hide-thumb");
+            bannerTitle.classList.add("small-title");
+        } else {
+            bannerThumb.classList.remove("hide-thumb");
+            bannerTitle.classList.remove("small-title");
+        }
+    });
+
+
+//SLIDER
+
+    const swiperInfo = new Swiper('.swiper-info', {
+        pagination: {
+            el: '.swiper-info .swiper-pagination',
+            clickable: true,
+        },
+    });
+
+    if (document.querySelector('.product-slider')) {
+        const productSlider = new Swiper('.product-slider ', {
+            pagination: {
+                el: '.product-slider .swiper-pagination',
+                clickable: true,
+            },
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+            },
+            breakpoints: {
+                577: {
+                    slidesPerView: 2,
+                    spaceBetween: 16
+                }
+            },
+        });
+    }
+    if (document.querySelector('.cards-slider')) {
+        const cardSlider = new Swiper('.cards-slider ', {
+            pagination: {
+                el: '.cards-slider .swiper-pagination',
+                clickable: true,
+            },
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+            },
+            breakpoints: {
+                577: {
+                    slidesPerView: 2,
+                    spaceBetween: 16,
+                }
+            },
+        });
+    }
+    if (document.querySelector('.green-slider')) {
+        const greenSlider = new Swiper('.green-slider', {
+            pagination: {
+                el: '.green-slider .swiper-pagination',
+                clickable: true,
+            },
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+            },
+        });
+    }
+    if (document.querySelector('.slider-4-col')) {
+        const slider4Col = new Swiper('.slider-4-col', {
+            slidesPerView: 1,
+            pagination: {
+                el: '.slider-4-col .swiper-pagination',
+                clickable: true,
+            },
+            navigation: {
+                nextEl: ".slider-gallery2 .swiper-button-next",
+                prevEl: ".slider-gallery2 .swiper-button-prev",
+            },
+            breakpoints: {
+                577: {
+                    slidesPerView: 2,
+                    spaceBetween: 16
+                },
+                767: {
+                    slidesPerView: 3,
+                    spaceBetween: 16
+                },
+                991: {
+                    slidesPerView: 4,
+                    spaceBetween: 16
+                }
+            },
+        });
+    }
+    if (document.querySelector('.slider-gallery2')) {
+        const sliderGallery2 = new Swiper('.slider-gallery2 ', {
+            spaceBetween: 16,
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+            },
+
+        });
+    }
+    window.addEventListener('load', function () {
+        if (document.querySelector('.gallery')) {
+            baguetteBox.run('.gallery', {});
+        }
+        if (document.querySelector('.gallery2')) {
+
+            baguetteBox.run('.gallery2', {});
+        }
+    });
+    //TABS
+    const tabLinks = document.querySelectorAll('.tabs__nav-link');
+    const tabContents = document.querySelectorAll('.tab-content');
+
+    tabLinks.forEach(link => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+
+            tabLinks.forEach(link => link.classList.remove('active'));
+            tabContents.forEach(content => content.classList.remove('active'));
+            link.classList.add('active');
+
+            const targetId = link.getAttribute('href').substring(1);
+            document.getElementById(targetId).classList.add('active');
+        });
+    });
+
+
+// POPUP
+    const popupCont = document.querySelector(".popup_contacts");
+    const btnCall = document.querySelector(".btn_call");
+    const contactsClose = document.querySelector('.popup_contacts .popup_close');
+    const backdrop = document.querySelector('.backdrop');
+
+    btnCall.addEventListener('click', () => {
+        popupCont.classList.add('open_modal');
+        backdrop.classList.toggle('el-visible');
+    });
+
+    const closePopup = () => {
+        popupCont.classList.remove('open_modal');
+        backdrop.classList.remove('el-visible');
+        if (window.clearContactForm) {
+            window.clearContactForm();
+        }
+    }
+
+    contactsClose.addEventListener('click', closePopup);
+    backdrop.addEventListener('click', closePopup);
+
+
+//    MULTI STEP FORM
+    const msForm = document.querySelector('.multi-step-form');
+
+    if (msForm) {
+        let activeIndex = 0;
+        const stepsCircle = msForm.querySelectorAll('.progress-step');
+        const stepContent = msForm.querySelectorAll('.form__step');
+
+        const btnsNext = msForm.querySelectorAll('.btn_next');
+        const prevBtns = msForm.querySelectorAll('.form__nav-link');
+
+        const thYouPage = msForm.querySelector('.thanks-you-page');
+
+        stepsCircle[activeIndex].classList.add('progress-step_active');
+        stepContent[activeIndex].classList.add('active');
+        prevBtns[activeIndex].classList.add('show');
+
+        const goToStep = (activeIndex) => {
+            stepContent.forEach(el => el.classList.remove('active'))
+
+            stepsCircle.forEach((el, index) => {
+                if (index > activeIndex) {
+                    el.classList.remove('progress-step_active')
+                } else {
+                    el.classList.add('progress-step_active')
+                }
+            })
+
+            prevBtns.forEach((el, index) => {
+                if (index > activeIndex) {
+                    el.classList.remove('show');
+                } else {
+                    el.classList.add('show');
+                }
+            })
+
+            const activeContent = stepContent[activeIndex];
+            if (activeContent) {
+                activeContent.classList.add('active')
+            }
+        }
+
+        window.clearContactForm = () => {
+            msForm.reset();
+            activeIndex = 0;
+            goToStep(activeIndex);
+            thYouPage.classList.remove('show');
+        }
+
+
+        btnsNext.forEach(btn => btn.addEventListener(
+            'click', (e) => {
+                activeIndex = activeIndex + 1;
+                if (activeIndex < stepContent.length) {
+                    goToStep(activeIndex);
+                }
+
+                if (e.target.type === 'submit') {
+                    e.preventDefault();
+
+                    thYouPage.classList.add('show');
+                    setTimeout(() => {
+                        closePopup();
+                    }, 3000)
+                }
+            }
+        ));
+
+        prevBtns.forEach(btn => btn.addEventListener(
+            'click', (e) => {
+                activeIndex = Array.from(prevBtns).findIndex(el => el === e.target);
+
+                goToStep(activeIndex);
+            }
+        ));
+
+        msForm.querySelectorAll('.js-form-field').forEach(el => {
+            el.addEventListener('input', (e) => {
+                const activeSetControls = stepContent[activeIndex].querySelectorAll('.js-form-field');
+                const submitBtn = stepContent[activeIndex].querySelector('.btn_next');
+
+                let disabled = true;
+                if (activeIndex === 0) {
+                    disabled = Array.from(activeSetControls).filter(el => el.hasAttribute('required')).some(el => el.value === '');
+                } else if (activeIndex === 1) {
+                    disabled = !Array.from(stepContent[activeIndex].querySelectorAll('[type="checkbox"]')).some(el => el.checked)
+                } else if (activeIndex === 2) {
+                    const otherCity = msForm.querySelector('[name="other_city"]');
+                    if (otherCity === e.target) {
+                        stepContent[activeIndex].querySelectorAll('[type="radio"]').forEach(el => el.checked = false)
+                    } else {
+                        otherCity.value = '';
+                    }
+
+                    disabled = !Array.from(stepContent[activeIndex].querySelectorAll('[type="radio"]')).some(el => el.checked) &&
+                        otherCity.value === '';
+
+                    console.log(msForm.querySelector('[name="other_city"]'))
+                }
+
+                submitBtn.disabled = disabled
+            });
+        })
+    }
+//    SIDEBAR HOVER OVERLAY
+    document.querySelectorAll('.sidebar__nav .dropdown').forEach(function (element) {
+        element.addEventListener('mouseenter', function () {
+            document.querySelector('.sidebar__overlay').classList.add('show');
+        });
+
+        element.addEventListener('mouseleave', function () {
+            document.querySelector('.sidebar__overlay').classList.remove('show');
+        });
+    });
+
+    document.querySelectorAll('#accordion .panel__heading').forEach((heading) => {
+        heading.addEventListener('click', function () {
+            const isOpen = this.classList.contains('open');
+            const allPanels = document.querySelectorAll('#accordion .panel__heading');
+            const allContents = document.querySelectorAll('.panel-collapse');
+
+            allPanels.forEach(panel => panel.classList.remove('open'));
+            allContents.forEach(content => content.style.maxHeight = null);
+
+            if (!isOpen) {
+                this.classList.add('open');
+                const panelContent = this.nextElementSibling;
+                panelContent.style.maxHeight = panelContent.scrollHeight + 'px';
+            }
+        });
+    });
+
+});
+
+
+
+
+
