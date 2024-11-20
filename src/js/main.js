6b6c88993dca6e998a08c263549962bc94a9b567
v-lavs <lavskavanda@gmail.com>
2024-11-20 15:55:03 +0200

    fixed js product feature

--- src/js/main.js
+++ src/js/main.js
@@ -216,7 +216,7 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 
-    jQuery('body').on('click', '.product-feature__thumb', function(){
+    jQuery('body').on('click', '.product-feature__thumb', function () {
 
         jQuery(this).siblings('.product-feature__thumb').removeClass('active');
         jQuery(this).parent().parent().find('.product-feature__content .tab-content').removeClass('active');
@@ -266,11 +266,11 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 //----------------------------------------------------
     //BLOCK FEATURE PRODUCT
-const  thumbNavBlock = document.querySelectorAll('.product-feature__nav');
+    const thumbNavBlock = document.querySelectorAll('.product-feature__nav');
     const tabThumbNav = document.querySelectorAll('.product-feature__thumb');
 
     tabThumbNav.forEach(link => {
-     if(window.innerWidth >767)   {
+        if (window.innerWidth > 767) {
             link.addEventListener('click', (e) => {
                 e.preventDefault();
                 const parent = findParent(e.target, 'product-feature');
@@ -286,22 +286,10 @@ const  thumbNavBlock = document.querySelectorAll('.product-feature__nav');
                 document.getElementById(targetId).classList.add('active');
             });
         } else {
-         if (thumbNavBlock) {
-             thumbNavBlock.style.display = 'none'; // Ховаємо навігацію
-         }
-
-         const targetId = link.getAttribute('data-target').replace('#', '');
-         const targetElement = document.getElementById(targetId);
-
-         if (targetElement) {
-             // Оновлюємо джерело мобільного зображення
-             const mobileThumb = targetElement.querySelector('.product-feature__thumb-mob img');
-             if (mobileThumb) {
-                 mobileThumb.src = link.getAttribute('data-src'); // Оновлюємо src
-                 mobileThumb.alt = link.querySelector('img').alt || 'Product Image'; // Оновлюємо alt
-             }
-         }
-     }
+            if (thumbNavBlock) {
+                thumbNavBlock.style.display = 'none'; // Ховаємо навігацію
+            }
+        }
     });
 
 //-------------------------------------------------------
